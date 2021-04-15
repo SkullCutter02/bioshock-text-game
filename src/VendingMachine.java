8ec13b3be664ea9583d4d4e32ab7d03fe216dfac
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-04-15 14:31:04 +0800

    Added sounds to vending machine

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class VendingMachine {
     private int coins;
-    private Inventory inventory;
+    private final Inventory inventory;
 
     private final Scanner scanner = new Scanner(System.in);
 
@@ -35,9 +35,15 @@ public class VendingMachine {
         if (items.get(itemName).getPrice() < coins)
             return true;
         else {
+            AudioManager.playSound("sounds/effects/vending_machine_no_money.wav");
             System.out.println("You don't have enough coins to buy this item!");
             return false;
         }
+    }
+
+    private void buyItemSuccessful() {
+        System.out.println("Item successfully bought!");
+        AudioManager.playSound("sounds/effects/vending_machine_no_refunds.wav");
     }
 
     private void buyItem(String itemName) {
@@ -45,31 +51,31 @@ public class VendingMachine {
             if (canBuy(PISTOL_AMMO)) {
                 coins -= items.get(PISTOL_AMMO).getPrice();
                 inventory.getWeapon("pistol").addAmmo(10);
-                System.out.println("Item successfully bought!");
+                buyItemSuccessful();
             }
         } else if (itemName.equalsIgnoreCase(MACHINE_GUN_AMMO)) {
             if (canBuy(MACHINE_GUN_AMMO)) {
                 coins -= items.get(MACHINE_GUN_AMMO).getPrice();
                 inventory.getWeapon("machine gun").addAmmo(50);
-                System.out.println("Item successfully bought!");
+                buyItemSuccessful();
             }
         } else if (itemName.equalsIgnoreCase(SHOTGUN_AMMO)) {
             if (canBuy(SHOTGUN_AMMO)) {
                 coins -= items.get(SHOTGUN_AMMO).getPrice();
                 inventory.getWeapon("shotgun").addAmmo(5);
-                System.out.println("Item successfully bought!");
+                buyItemSuccessful();
             }
         } else if (itemName.equalsIgnoreCase(HEALTH_PACK)) {
             if (canBuy(HEALTH_PACK)) {
                 coins -= items.get(HEALTH_PACK).getPrice();
                 inventory.addHealthPack();
-                System.out.println("Item successfully bought!");
+                buyItemSuccessful();
             }
         } else if (itemName.equalsIgnoreCase(EVE_HYPO)) {
             if (canBuy(EVE_HYPO)) {
                 coins -= items.get(EVE_HYPO).getPrice();
                 inventory.addEveHypo();
-                System.out.println("Item successfully bought!");
+                buyItemSuccessful();
             }
         } else {
             System.out.println("Item with the name " + itemName + " is not found!");
@@ -79,6 +85,7 @@ public class VendingMachine {
     public void enterMode() {
         System.out.println();
         System.out.println("You found a vending machine. You have now entered vending machine mode");
+        AudioManager.playSound("sounds/effects/vending_machine_circus_of_values.wav");
 
         while (true) {
             System.out.println("You have " + coins + " coins");
@@ -86,9 +93,10 @@ public class VendingMachine {
                     "Type inventory to display your inventory, and get-info <item name> to view information about an item in your inventory. Type QUIT to quit vending machine mode.");
             String input = scanner.nextLine().toLowerCase().trim();
 
-            if (input.equals("quit"))
+            if (input.equals("quit")) {
+                AudioManager.playSound("sounds/effects/vending_machine_quit.wav");
                 break;
-            else if (input.equals("items"))
+            } else if (input.equals("items"))
                 displayItems();
             else if (input.split(" ")[0].equals("buy") && input.split(" ").length >= 2)
                 buyItem(input.split(" ", 2)[1]);
